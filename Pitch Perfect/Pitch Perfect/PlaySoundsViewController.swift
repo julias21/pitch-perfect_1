1d7be15232b602519611b9a9e0df0a1293001fb8
julias21 <juliastefani@gmail.com>
2015-04-28 23:29:58 -0700

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,90 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Julia Stefani on 4/20/15.
+//  Copyright (c) 2015 Julia Stefani. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    var audioPlayer:AVAudioPlayer!
+    var receivedAudio:RecordedAudio!
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
+
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
+        audioPlayer.enableRate = true
+        audioEngine = AVAudioEngine()
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func PlaySoundsSnail(sender: UIButton) {
+        audioPlayer.rate = 0.5
+        audioPlayer.currentTime = 0.0
+        audioEngine.stop()
+        audioEngine.reset()
+        audioPlayer.play()
+        
+    }
+    @IBAction func PlaySoundsFast(sender: UIButton) {
+        audioPlayer.rate = 2.0
+        audioPlayer.currentTime = 0.0
+        audioEngine.stop()
+        audioEngine.reset()
+        audioPlayer.play()
+    }
+    
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
+    
+    @IBAction func playDarthVadorAudio(sender: UIButton) {
+        playAudioWithVariablePitch(-1000)
+    }
+    
+    
+    //Function to change pitch
+    func playAudioWithVariablePitch(pitch: Float){
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
+    }
+    
+    @IBAction func StopPlayingSounds(sender: UIButton) {
+       audioPlayer.stop()
+       audioEngine.stop()
+    }
+    
+
+}
